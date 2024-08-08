8fb52322b8f28fcdbdecd6ecafe94b8089c5eb40
Team-12DN <danielyo7216@gmail.com>
2024-08-09 00:53:20 +0300

    clean movment and food collusion

--- Snakegame-function.js
+++ Snakegame-function.js
@@ -1,10 +1,13 @@
 // ------Under development--------
 const shankehade = document.getElementById("shankehade");
-const moveamount = 10;
+const Shankfood = document.getElementById("Shankfood");
+const scorediplay = document.getElementById("score");
+const moveamount = 20;
+
 let x = 0;
 let y = 0;
-let curentx = x;
-let curenty = y;
+let Shankfoodx;
+let Shankfoody;
 let moveupid = null;
 let movedownid = null;
 let moveleftid = null;
@@ -12,24 +15,61 @@ let moverightid = null;
 let up, down, right, left;
 let speed = 100;
 let privkey;
+let newkey;
+let score = 0;
+
+Shankfood.style.top = `${Randomreturn()}px`;
+Shankfood.style.left = `${Randomreturn()}px`;
+
+var numb = Shankfood.style.left.match(/\d/g);
+Shankfoodx = numb.join("");
+var numb = Shankfood.style.top.match(/\d/g);
+Shankfoody = numb.join("");
+
+function Randomreturn() {
+	let array = [];
+	for (let index = 1; index < 21; index++) {
+		let seq = 20 * (index - 1);
+		array.push(seq);
+	}
+	return array[Math.floor(Math.random() * 20)];
+}
+
 document.addEventListener("keydown", (event) => {
 	if (event.key.startsWith("Arrow")) {
 		event.preventDefault();
 
 		switch (event.key) {
 			case "ArrowUp":
-				move((up = 1), (down = 0), (right = 0), (left = 0));
+				newkey = event.key;
+				if (privkey == newkey || privkey == "ArrowDown") {
+				} else {
+					move((up = 1), (down = 0), (right = 0), (left = 0));
+				}
 				break;
 			case "ArrowDown":
-				move((up = 0), (down = 1), (right = 0), (left = 0));
+				newkey = event.key;
+				if (privkey == newkey || privkey == "ArrowUp") {
+				} else {
+					move((up = 0), (down = 1), (right = 0), (left = 0));
+				}
 				break;
 			case "ArrowRight":
-				move((up = 0), (down = 0), (right = 1), (left = 0));
+				newkey = event.key;
+				if (privkey == newkey || privkey == "ArrowLeft") {
+				} else {
+					move((up = 0), (down = 0), (right = 1), (left = 0));
+				}
 				break;
 			case "ArrowLeft":
-				move((up = 0), (down = 0), (right = 0), (left = 1));
+				newkey = event.key;
+				if (privkey == newkey || privkey == "ArrowRight") {
+				} else {
+					move((up = 0), (down = 0), (right = 0), (left = 1));
+				}
 				break;
 		}
+		privkey = event.key;
 
 		Colusion();
 	}
@@ -77,21 +117,38 @@ function move(up, down, left, right) {
 		clearInterval(movedownid);
 	}
 }
-function assign() {    
+function assign() {
+	shankehade.style.transition = "0.05s";
 	shankehade.style.top = `${y}px`;
 	shankehade.style.left = `${x}px`;
 }
 
 function Colusion() {
 	if (x >= 400 || x < 0 || y >= 400 || y < 0) {
-		shankehade.style.top = 0;
-		shankehade.style.left = 0;
 		x = 0;
 		y = 0;
-        clearInterval(moveleftid);
+		clearInterval(moveleftid);
 		clearInterval(moveupid);
 		clearInterval(movedownid);
-        clearInterval(moverightid);
+		clearInterval(moverightid);
+		Shankfood.style.top = `${Randomreturn()}px`;
+		Shankfood.style.left = `${Randomreturn()}px`;
+		numb = Shankfood.style.left.match(/\d/g);
+		Shankfoodx = numb.join("");
+		numb = Shankfood.style.top.match(/\d/g);
+		Shankfoody = numb.join("");
+		score = 0;
+		scorediplay.textContent = `score: ${score}`;
+	}
 
+	if (x == Shankfoodx && y == Shankfoody) {
+		Shankfood.style.top = `${Randomreturn()}px`;
+		Shankfood.style.left = `${Randomreturn()}px`;
+		numb = Shankfood.style.left.match(/\d/g);
+		Shankfoodx = numb.join("");
+		numb = Shankfood.style.top.match(/\d/g);
+		Shankfoody = numb.join("");
+		score++;
+		scorediplay.textContent = `score: ${score}`;
 	}
 }
